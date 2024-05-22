9fbbfe66ed2328422701ae7fd8a13695c2ad6f66
Juan Gomez <juan.e.g.f@outlook.com>
2024-05-21 23:31:44 -0600

    chore: Add ktor dependency

--- shared/networking/build.gradle.kts
+++ shared/networking/build.gradle.kts
@@ -2,8 +2,14 @@ plugins {
     alias(libs.plugins.okik.shared.kpp)
 }
 
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.ktor.client.core)
+        }
+    }
+}
+
 android {
     namespace = "okik.tech.community.admin.shared.networking"
-
-
 }
