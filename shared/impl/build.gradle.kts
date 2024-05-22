e8c5bdfdc7d4136b8d070bd1d43ec11f804abc00
Juan Gomez <juan.e.g.f@outlook.com>
2024-05-22 00:34:22 -0600

    chore: Add impl module

---
+++ shared/impl/build.gradle.kts
@@ -0,0 +1,9 @@
+import dev.icerock.gradle.MRVisibility
+
+plugins {
+    alias(libs.plugins.okik.shared.kpp)
+}
+
+android {
+    namespace = "okik.tech.community.admin.shared"
+}
--- shared/storage/src/commonMain/kotlin/okik/tech/community/admin/shared/storage/Client.kt
+++
@@ -1,2 +0,0 @@
-package okik.tech.community.admin.shared
-
