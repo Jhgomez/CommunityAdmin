f2aabf5bae68b374c5e0908e24c8569c9fa01fb9
Juan Gomez <juan.e.g.f@outlook.com>
2024-05-30 00:08:14 -0600

    refactor: remove expect modifier

--- shared/networking/src/commonMain/kotlin/okik/tech/community/admin/shared/networking/datasources/AuthenticationDataSource.kt
+++ shared/networking/src/commonMain/kotlin/okik/tech/community/admin/shared/networking/datasources/AuthenticationDataSource.kt
@@ -1,6 +1,6 @@
 package okik.tech.community.admin.shared.networking.datasources
 
-expect interface AuthenticationDataSource {
+interface AuthenticationDataSource {
     fun signUp()
     fun logIn()
     fun logOut()
